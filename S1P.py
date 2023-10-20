f8aea6677e588e3f73ad5537a2f4c237f9bd2dd1
0scar457 <146883791+0scar457@users.noreply.github.com>
2023-10-20 14:19:30 -0700

    Add files via upload

---
+++ S1P.py
@@ -0,0 +1,21 @@
+#This is the Session 1 prompt 
+
+
+def Printmyinfo():
+	name=("Oscar Lopez")
+	pronouns=("He/Him")
+	moive=("I always liked watching Cars as a kid")
+	food=("I love eating Poke")
+
+	print(name)
+	print(pronouns)
+	print(moive)
+	print(food)
+
+
+def main():
+	Printmyinfo()
+
+
+if __name__=="__main__":
+	main()
