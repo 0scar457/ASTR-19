8bb39fd6f3d5efb5d0947c94e0e3b71eb264d834
0scar457 <146883791+0scar457@users.noreply.github.com>
2023-10-20 14:51:41 -0700

    Add files via upload

---
+++ S2P.py
@@ -0,0 +1,23 @@
+def main():
+    # Input values
+    fnumber1 = 10
+    fnumber2 = 5
+    int_num1 = 15
+    int_num2 = 20
+
+    #adding the numbers
+    sum_result = fnumber1 + fnumber2
+
+    # subtracing the numbers 
+    difference_result = int_num1 - int_num2
+
+    # muliplying float
+    product_result = fnumber1 * int_num1
+
+    # Printing the results 
+    print(f"Sum of {fnumber1} and {fnumber2} is {sum_result}")
+    print(f"Difference between {int_num1} and {int_num2} is {difference_result}")
+    print(f"Product of {fnumber1} and {int_num1} is {product_result}")
+
+if __name__ == "__main__":
+    main()
